dba28e118fd477ffeffb06a3dba2a557b645b148
billy4479 <giachi.ellero@gmail.com>
2022-11-09 23:06:17 +0100

    Add blake3::hash function

--- blake3-bindings/src/lib.rs
+++ blake3-bindings/src/lib.rs
@@ -1,3 +1,12 @@
+struct Hash;
+
+#[no_mangle]
+pub extern "C" fn hash(data: *const u8, length: usize) -> *mut blake3::Hash {
+    unsafe {
+        let input = std::slice::from_raw_parts(data, length);
+        Box::into_raw(Box::new(blake3::hash(input)))
+    }
+}
 
 #[no_mangle]
 pub extern "C" fn add(left: usize, right: usize) -> usize {
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include "MyClass.hpp"
+#include <array>
 #include <blake3.h>
 
 int main() {
+
+    std::array<u8, 32> data;
+    data.fill(0);
+
+    auto h = blake3::hash(data.data(), data.size());
+    (void)h;
+
     MyClass my_class(42);
 
     std::cout << "Hello World!" << std::endl
