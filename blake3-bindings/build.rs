039df676c5176a8d1da43ea54ab2b562cb8de0f3
billy4479 <giachi.ellero@gmail.com>
2022-11-09 20:21:52 +0100

    Add namesapce for generated symbols

--- blake3-bindings/build.rs
+++ blake3-bindings/build.rs
@@ -3,12 +3,15 @@ use std::env;
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
-    let out_file = format!("{}/blake3.h", std::env::var("BUILD_DIR").unwrap_or(String::from(".")));
-
+    let out_file = format!(
+        "{}/blake3.h",
+        std::env::var("BUILD_DIR").unwrap_or(String::from("."))
+    );
 
     cbindgen::Builder::new()
-      .with_crate(crate_dir)
-      .generate()
-      .expect("Unable to generate bindings")
-      .write_to_file(out_file);
+        .with_namespace("blake3")
+        .with_crate(crate_dir)
+        .generate()
+        .expect("Unable to generate bindings")
+        .write_to_file(out_file);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ int main() {
     MyClass my_class(42);
 
     std::cout << "Hello World!" << std::endl
-              << add(my_class.get_my_number(), my_class.get_my_number())
+              << blake3::add(my_class.get_my_number(), my_class.get_my_number())
               << std::endl;
 
     return 0;
