80aa419e227998aa0017992fdbf5bc2a16d4aaf8
billy4479 <giachi.ellero@gmail.com>
2022-11-10 16:46:09 +0100

    Update test program

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,14 +1,24 @@
 #include <Hash.hpp>
+#include <algorithm>
 #include <array>
+#include <filesystem>
+#include <fstream>
+#include <vector>
 
-int main() {
+int main(i32 argc, char **argv) {
 
-    std::array<u8, 32> data;
-    data.fill(0);
+    if (argc != 2)
+        exit(1);
 
-    auto hash = blake3::Hash(data);
+    namespace fs = std::filesystem;
 
-    std::cout << "Hello World!" << std::endl << hash.as_hex() << std::endl;
+    auto path = fs::path(argv[1]);
+    auto file = std::ifstream(path, std::ios::in | std::ios::binary);
+    auto vec = std::vector<u8>((std::istreambuf_iterator<char>(file)),
+                               std::istreambuf_iterator<char>());
+
+    auto hash = blake3::Hash(vec);
+    std::cout << hash.as_hex() << std::endl;
 
     return 0;
 }
